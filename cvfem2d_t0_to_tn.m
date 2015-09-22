5b2e780b2a1a0c8257f6dc4f40103bb741f05bf4
Minho Park <waps00@gmail.com>
2015-09-22 11:24:46 +0100

    Added visulalization in cvfem2d_t0_to_tn.

--- cvfem2d_t0_to_tn.m
+++ cvfem2d_t0_to_tn.m
@@ -1,10 +1,8 @@
 function opt = cvfem2d_t0_to_tn(opt,tn)
-%  CVFEM2D 2D control volume finite elment method.
-%     opt = CVFEM2D(opt) solves moving boundary problems using the control volume finite
-%     element method.
-%     
-%     See also cvfem_setup
-% todo cvfem2d_vis_timestep, update-flow_rate_tri
+
+if tn <= opt.cvfem.last_tn
+    error('tn must be greater than last_tn')
+end
 gD = str2func(opt.bndry.gd_filename);
 opt.cvfem.u = zeros(opt.mesh.nnode,1);
 opt.cvfem.u(opt.bndry.inlet_flag==1) = gD(opt.bndry.inlet_pos);
@@ -15,11 +13,11 @@ opt.cvfem.voidID = ones(opt.mesh.nnode,1);
 % Initialise the activeNode and activeElement vectors
 opt = cvfem2d_init(opt);
 
-% if opt.vis.dumpFlag == 1 && opt.last_timestep < opt.fTime
-%     opt.vis.fTimes(opt.vis.dumpIdx) = opt.cvfem.fTime;
-%     cvfem2d_vis_timestep(opt);
-%     opt.vis.dumpIdx = opt.vis.dumpIdx + 1;
-% end
+if opt.vis.dumpFlag == 1 && opt.cvfem.last_tn == 0
+    opt.vis.fTimes(opt.vis.dumpIdx) = opt.cvfem.fTime;
+    cvfem2d_vis_timestep(opt);
+    opt.vis.dumpIdx = opt.vis.dumpIdx + 1;
+end
 Q   = update_flow_rate_tri(opt);
 
 dt  = compute_time_increment(Q,opt.cvfem.fFactor,opt.cvfem.V);
@@ -28,11 +26,11 @@ opt.cvfem.fTime = increase_fTime(opt.cvfem.fTime,dt);
     = update_filling_factor(opt.cvfem.fFactor,Q,dt,opt.cvfem.V);
 
 
-% if opt.vis.dumpFlag == 1 && opt.last_timestep < opt.fTime
-%     fTimes(opt.vis.dumpIdx) = opt.cvfem.fTime;
-%     cvfem2d_vis_timestep(opt);
-%     opt.vis.dumpIdx = opt.vis.dumpIdx + 1;
-% end
+if opt.vis.dumpFlag == 1 && opt.cvfem.last_tn < opt.cvfem.fTime
+    opt.vis.fTimes(opt.vis.dumpIdx) = opt.cvfem.fTime;
+    cvfem2d_vis_timestep(opt);
+    opt.vis.dumpIdx = opt.vis.dumpIdx + 1;
+end
 u_old = opt.cvfem.u;
 Q_old = Q;
 while ~isFilled(opt.cvfem.fFactor,opt.mesh.nnode)
@@ -46,6 +44,7 @@ while ~isFilled(opt.cvfem.fFactor,opt.mesh.nnode)
     end
     opt.cvfem.fTime = increase_fTime(opt.cvfem.fTime,dt);
     
+    
     if opt.cvfem.fTime > tn
         opt.cvfem.u = (tn - opt.cvfem.fTime+dt)/dt*opt.cvfem.u + ...
             (opt.cvfem.fTime-tn)/dt*u_old;
@@ -53,15 +52,20 @@ while ~isFilled(opt.cvfem.fFactor,opt.mesh.nnode)
         Q_old(Q_old<0) = 0;
         opt.cvfem.Q = (tn-opt.cvfem.fTime+dt)/dt*Q + (opt.cvfem.fTime-tn)/dt*Q_old;
         opt.cvfem.fTime = tn;
-        
+        if opt.vis.dumpFlag == 1 && opt.cvfem.last_tn < opt.cvfem.fTime
+        opt.vis.fTimes(opt.vis.dumpIdx) = opt.cvfem.fTime;
+        cvfem2d_vis_timestep(opt);
+        opt.vis.dumpIdx = opt.vis.dumpIdx + 1;
+        opt.cvfem.last_tn = opt.cvfem.fTime;
+    end
         break;
         
     end
-%     if opt.vis.dumpFlag == 1
-%         fTimes(opt.vis.dumpIdx) = opt.cvfem.fTime;
-%         cvfem2d_vis_timestep(opt);
-%         opt.vis.dumpIdx = opt.vis.dumpIdx + 1;
-%     end
+    if opt.vis.dumpFlag == 1 && opt.cvfem.last_tn < opt.cvfem.fTime
+        opt.vis.fTimes(opt.vis.dumpIdx) = opt.cvfem.fTime;
+        cvfem2d_vis_timestep(opt);
+        opt.vis.dumpIdx = opt.vis.dumpIdx + 1;
+    end
 
     [opt.cvfem.fFactor, opt.cvfem.new_filled_volume] ...
         = update_filling_factor(opt.cvfem.fFactor,Q,dt,opt.cvfem.V);
@@ -69,19 +73,5 @@ while ~isFilled(opt.cvfem.fFactor,opt.mesh.nnode)
     Q_old = Q;
 end
 
-% if opt.vis.dumpFlag == 1
-%     
-%     opt.vis.dumpIdx = opt.vis.dumpIdx - 1;
-%   
-%     [flag, ME,MID] = mkdir(opt.vis.filename);
-%     if strcmp(MID,'MATLAB:MKDIR:DirectoryExists')
-%         system(['rm -rf ' opt.vis.filename]);
-%         mkdir(opt.vis.filename);
-%     end
-%     
-%     cwd = pwd;
-%     movefile('*.vtu',[cwd filesep opt.vis.filename])
-%     cvfem2d_vis_global(fTimes(1:opt.vis.dumpIdx),opt.vis.filename)
-%     
-% end
+
 
